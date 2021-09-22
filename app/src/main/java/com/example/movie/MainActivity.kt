0353841f93a7d345c674f2cc80d4b466f4ff6d2e
Kiichel <kiichel@mail.ru>
2021-09-22 16:43:00 +0300

    Added splash screen

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -1,18 +1,40 @@
 package com.example.movie
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.icu.number.Scale
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.ScaleAnimation
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    //gvQKO6QDtVxq2DAVz1euMcRM5TXRgsYh
-
+    lateinit var handler: Handler
+    private lateinit var anim: ScaleAnimation
+    lateinit var img: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        img = findViewById(R.id.imgIconId)
 
-        val i = Intent(this, MoviesListActivity::class.java)
-        startActivity(i)
+        anim = ScaleAnimation(0F, 1F, 0F, 1F,
+        img.measuredHeight.toFloat(), img.measuredWidth.toFloat())
+
+        anim.duration = 1000
+        anim.interpolator = DecelerateInterpolator()
+        anim.fillAfter = true
+        img.startAnimation(anim)
+
+        handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val i = Intent(this, MoviesListActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 1000)
     }
 }
