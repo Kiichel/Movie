cfa2ad3397d32396dd140c9cf75d6235723fba0a
Kiichel <kiichel@mail.ru>
2021-09-22 16:56:11 +0300

    Created dev branch

--- app/src/main/java/com/example/movie/Common.kt
+++ app/src/main/java/com/example/movie/Common.kt
@@ -6,7 +6,7 @@ import com.example.movie.retrofit.RetrofitServices
 object Common {
     private val BASE_URL =
         "https://api.nytimes.com/svc/movies/v2/reviews/"
-//?api-key=gvQKO6QDtVxq2DAVz1euMcRM5TXRgsYh
+
     val retrofitService: RetrofitServices
     get() = RetrofitClient.getClient(BASE_URL).create(RetrofitServices::class.java)
 }
